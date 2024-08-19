f34036b514f64068278bada284364a5d6961f8fe
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-19 17:47:25 +0000

    updated the __init__.py file

--- database/taskmanager/__init__.py
+++ database/taskmanager/__init__.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:  # Fixed indentation
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
